61733d7459f72f3e39bcb64d34e4b7bf73994a1e
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-03 19:26:32 +0300

    Update User.cpp

--- ModaElectronicCommerceSystem/User.cpp
+++ ModaElectronicCommerceSystem/User.cpp
@@ -1,1 +1,20 @@
 #include "User.h"
+
+User::User(const MyString& name, const MyString& egn, const MyString& password) :name(name), egn(egn), password(password)
+{
+
+}
+
+void User::viewProfile() const
+{
+
+}
+
+void User::logout()
+{
+}
+
+void User::help() const
+{
+
+}
