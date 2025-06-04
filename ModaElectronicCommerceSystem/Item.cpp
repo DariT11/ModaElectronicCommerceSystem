fedc88ca0bdb2fd89468fbb73ac98b91402b43d0
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-04 10:40:27 +0300

    Update Item.cpp

---
+++ ModaElectronicCommerceSystem/Item.cpp
@@ -0,0 +1,23 @@
+#include "Item.h"
+
+void Item::updateAvailability()
+{
+}
+
+Item::Item(const MyString& name, double price, unsigned quantity, const MyString& description, double rating)
+{
+}
+
+void Item::view() const
+{
+}
+
+bool Item::isAvailable() const
+{
+    return false;
+}
+
+bool Item::purchase(unsigned count)
+{
+    return false;
+}
