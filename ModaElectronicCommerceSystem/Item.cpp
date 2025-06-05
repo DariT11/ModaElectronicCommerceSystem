b66db73b0aa0b578b4000675449c6309c5ef2d89
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-05 18:22:05 +0300

    Update Item.cpp

--- ModaElectronicCommerceSystem/Item.cpp
+++ ModaElectronicCommerceSystem/Item.cpp
@@ -2,10 +2,13 @@
 
 void Item::updateAvailability()
 {
+
 }
 
-Item::Item(const MyString& name, double price, unsigned quantity, const MyString& description, double rating)
+Item::Item(const MyString& name, double price, unsigned quantity, const MyString& description, double rating, size_t salesCount)
+    :name(name), price(price), quantity(quantity), description(description), rating(rating), salesCount(salesCount)
 {
+    availability = quantity > 0;
 }
 
 void Item::view() const
@@ -20,4 +23,44 @@ bool Item::isAvailable() const
 bool Item::purchase(unsigned count)
 {
     return false;
+}
+
+unsigned Item::getId() const
+{
+    return id;
+}
+
+const MyString& Item::getName() const
+{
+    return name;
+}
+
+double Item::getPrice() const
+{
+    return price;
+}
+
+unsigned Item::getQuantity() const
+{
+    return quantity;
+}
+
+const MyString& Item::getDescription() const
+{
+    return description;
+}
+
+double Item::getRating() const
+{
+    return rating;
+}
+
+bool Item::getAvailability() const
+{
+    return availability;
+}
+
+size_t Item::getSalesCount() const
+{
+    return salesCount;
 }
