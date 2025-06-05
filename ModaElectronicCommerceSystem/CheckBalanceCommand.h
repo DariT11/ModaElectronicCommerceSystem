c505f5adb3734a7e89ab06304141466d46195940
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-05 16:41:45 +0300

    Update CheckBalanceCommand.h

--- ModaElectronicCommerceSystem/CheckBalanceCommand.h
+++ ModaElectronicCommerceSystem/CheckBalanceCommand.h
@@ -1,8 +1,15 @@
 #pragma once
 #include "Command.h"
+#include "FinanceManager.h"
+#include "Client.h"
 
 class CheckBalanceCommand :public Command
 {
-	
+	FinanceManager& fManager;
+	Client& client;
+
+public:
+	CheckBalanceCommand(FinanceManager& fManager, Client& client);
+	void execute() override;
 };
 
