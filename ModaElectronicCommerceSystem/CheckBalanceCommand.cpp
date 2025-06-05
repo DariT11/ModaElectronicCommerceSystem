ad98bcdb67948271db145c1c742bb3132a2acd55
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-05 16:41:49 +0300

    Update CheckBalanceCommand.cpp

--- ModaElectronicCommerceSystem/CheckBalanceCommand.cpp
+++ ModaElectronicCommerceSystem/CheckBalanceCommand.cpp
@@ -1,1 +1,12 @@
 #include "CheckBalanceCommand.h"
+
+CheckBalanceCommand::CheckBalanceCommand(FinanceManager& fManager, Client& client)
+	:fManager(fManager), client(client)
+{
+
+}
+
+void CheckBalanceCommand::execute()
+{
+	fManager.checkBalance(client);
+}
