5bbc82038182da7a0ca6d1ed2308515239b55fa0
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-05 16:41:19 +0300

    Update ProductManager.cpp

--- ModaElectronicCommerceSystem/ProductManager.cpp
+++ ModaElectronicCommerceSystem/ProductManager.cpp
@@ -1,1 +1,16 @@
 #include "ProductManager.h"
+
+void ProductManager::listProducts(Item* const* items, size_t count) const
+{
+
+}
+
+void ProductManager::filterByRating(Item** items, size_t count) const
+{
+
+}
+
+void ProductManager::viewProduct(Item* const* items, size_t count, unsigned id) const
+{
+
+}
