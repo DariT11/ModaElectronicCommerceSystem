6f99a0c432c4767a3c6c1102b6c42e0302169b4f
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-05 16:41:16 +0300

    Update ProductManager.h

--- ModaElectronicCommerceSystem/ProductManager.h
+++ ModaElectronicCommerceSystem/ProductManager.h
@@ -6,4 +6,5 @@ class ProductManager
 public:
 	void listProducts(Item* const* items, size_t count) const;
 	void filterByRating(Item** items, size_t count) const;
+	void viewProduct(Item* const* items, size_t count, unsigned id) const;
 };
