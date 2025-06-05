fd3d1ab3c6b2a74edf209674009c62c8b1d19e4c
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-06 00:30:55 +0300

    Update Item.cpp

--- ModaElectronicCommerceSystem/Item.cpp
+++ ModaElectronicCommerceSystem/Item.cpp
@@ -1,28 +1,58 @@
 #include "Item.h"
+#include <stdexcept>
+using namespace std;
+
+unsigned Item::nextId = 1;
 
 void Item::updateAvailability()
 {
-
+    availability = quantity > 0;
 }
 
 Item::Item(const MyString& name, double price, unsigned quantity, const MyString& description, double rating, size_t salesCount)
     :name(name), price(price), quantity(quantity), description(description), rating(rating), salesCount(salesCount)
 {
+    id = nextId++;
     availability = quantity > 0;
 }
 
 void Item::view() const
 {
+    cout << "Item number: " << id << " | " 
+    << "Name:" << name.c_str() << " | "
+    << "Price: " << price << " BGN " << " | "
+    << "Rating: " << rating << "stars " << " | "
+    << "Availability " <<( (availability) ? "Yes:) " : "No:( " ) << std::endl;
 }
 
-bool Item::isAvailable() const
+void Item::printDetails() const
 {
-    return false;
+    cout << "Item number: " << id << endl;
+    cout << "Name: " << name.c_str() << endl;
+    cout << "Price: " << price << " BGN " << endl;
+    cout << "Quantity: " << quantity << endl;
+    cout << "Description: " << description.c_str() << endl;
+    cout << "Rating: " << rating << "stars " << endl;
+    cout << "Availability " << ((availability) ? "Yes:) " : "No:( ") << endl;
+    cout << "Number of sales: " << salesCount << endl;
 }
 
 bool Item::purchase(unsigned count)
 {
-    return false;
+    if (!availability)
+    {
+        throw std::out_of_range("This product is out of stock! :(");
+    }
+
+    if (quantity < count)
+    {
+        throw std::out_of_range("Insufficient availability! :)");
+    }
+    
+    quantity -= count;
+    updateAvailability();
+
+    return price * count;
 }
 
 unsigned Item::getId() const
