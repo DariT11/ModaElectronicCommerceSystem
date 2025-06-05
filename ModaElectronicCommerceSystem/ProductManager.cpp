5036bd3f6c68b7fbbefa34c6d6412b24d09fe8c8
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-05 18:21:52 +0300

    Update ProductManager.cpp

--- ModaElectronicCommerceSystem/ProductManager.cpp
+++ ModaElectronicCommerceSystem/ProductManager.cpp
@@ -1,13 +1,25 @@
 #include "ProductManager.h"
+using namespace std;
 
 void ProductManager::listProducts(Item* const* items, size_t count) const
 {
-
+	for (size_t i = 0; i < count; i++) //++i
+	{
+		
+	}
 }
 
 void ProductManager::filterByRating(Item** items, size_t count) const
 {
 
+}
+
+void ProductManager::filterByPrice(Item** items, size_t count, bool ascending) const
+{
+}
+
+void ProductManager::filterAlphabetically(Item** items, size_t count) const
+{
 }
 
 void ProductManager::viewProduct(Item* const* items, size_t count, unsigned id) const
