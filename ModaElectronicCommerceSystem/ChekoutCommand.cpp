108627b846f3d0cdaa9fe0bf6471a40163459d7f
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-11 00:06:52 +0300

    Create ChekoutCommand.cpp

---
+++ ModaElectronicCommerceSystem/ChekoutCommand.cpp
@@ -0,0 +1,27 @@
+#include "ChekoutCommand.h"
+#include "Order.h"
+using namespace std;
+
+ChekoutCommand::ChekoutCommand(Client& client, BusinessProfile& businessP)
+	:client(client), businessP(businessP)
+{
+}
+
+void ChekoutCommand::execute()
+{
+	/*double total = client.getCart().calculateTotalWithDiscount();
+
+	if (client.getWallet() < total)
+	{
+		cout << "Not enough money in the wallet! Please charge! :)" << endl;
+		return;
+	}
+
+	client.deductFromWallet(total);
+	Order order = client.getCart().toOrder();
+	businessP.receiveOrder(order);
+
+	client.getCart().clear();*/
+
+	cout << "Order sent for confirmation! Thank you:)" << endl;
+}
