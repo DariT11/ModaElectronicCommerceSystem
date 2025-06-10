f4e69df5adcbcd0b5ae75de2082a33f704544b56
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-11 00:07:19 +0300

    Create AddToCartCommand.h

---
+++ ModaElectronicCommerceSystem/AddToCartCommand.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Command.h"
+#include "Client.h"
+#include "ItemsCatalog.h"
+
+class AddToCartCommand: public Command
+{
+	Client& client;
+	ItemsCatalog& catalog;
+	unsigned itemId;
+	unsigned quantity;
+
+public:
+	AddToCartCommand(Client& client, ItemsCatalog& catalog, unsigned itemId, unsigned quantity);
+	void execute() override;
+};
+
