e6e541e325ff617f0a5d73aecb349a1ab9a42275
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-11 00:08:33 +0300

    Update ApplyDiscountCommand.cpp

---
+++ ModaElectronicCommerceSystem/ApplyDiscountCommand.cpp
@@ -0,0 +1,12 @@
+#include "ApplyDiscountCommand.h"
+
+ApplyDiscountCommand::ApplyDiscountCommand(Client& cliet)
+	:client(client)
+{
+
+}
+
+void ApplyDiscountCommand::execute()
+{
+	//client.getCard().applyDiscount(client.getPoints());
+}
