db131a11b58a9a2e1508c81311436f0532971eda
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-11 14:35:56 +0300

    Update ItemsCatalog.cpp

--- ModaElectronicCommerceSystem/ItemsCatalog.cpp
+++ ModaElectronicCommerceSystem/ItemsCatalog.cpp
@@ -1,1 +1,36 @@
 #include "ItemsCatalog.h"
+
+void ItemsCatalog::addItem(const Item& item)
+{
+    //
+}
+
+const MyVector<Item>& ItemsCatalog::getAllItems() const
+{
+    // TODO: insert return statement here
+}
+
+Item* ItemsCatalog::getItemById(unsigned Id)
+{
+    return nullptr;
+}
+
+void ItemsCatalog::listOfItems() const
+{
+}
+
+void ItemsCatalog::viewItem() const
+{
+}
+
+void ItemsCatalog::filterByRating()
+{
+}
+
+void ItemsCatalog::fileterByPrice()
+{
+}
+
+void ItemsCatalog::filterAlphabetically()
+{
+}
