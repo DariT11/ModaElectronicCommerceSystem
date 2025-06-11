16fe281b484b049d87bb040c4beca45e90a64237
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-11 14:36:00 +0300

    Update ItemsCatalog.h

--- ModaElectronicCommerceSystem/ItemsCatalog.h
+++ ModaElectronicCommerceSystem/ItemsCatalog.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "MyVector.hpp"
+#include "Item.h"
+
 class ItemsCatalog
 {
-	//Хетерогенен контейнер
+	MyVector<Item> items;
+
+public:
+	void addItem(const Item& item);
+	const MyVector<Item>& getAllItems() const;
+	Item* getItemById(unsigned Id);
+	void listOfItems() const;
+	void viewItem() const;
+
+	void filterByRating();
+	void fileterByPrice();
+	void filterAlphabetically();
 };
 
