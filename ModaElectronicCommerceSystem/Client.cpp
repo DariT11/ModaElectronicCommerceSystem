96b866e3ec76c007271dc0a1e5b88c49baa15543
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-11 14:35:48 +0300

    Update Client.cpp

--- ModaElectronicCommerceSystem/Client.cpp
+++ ModaElectronicCommerceSystem/Client.cpp
@@ -1,1 +1,51 @@
 #include "Client.h"
+
+Cart& Client::getCart() 
+{
+    return cart;
+}
+
+double Client::getWallet() const
+{
+    return wallet;
+}
+
+unsigned Client::getPoints() const
+{
+    return points;
+}
+
+Role Client::getRole() 
+{
+    return role;
+}
+
+MyString Client::getUsername() const
+{
+    return User::getUsername();
+}
+
+void Client::deductFromWallet(double amount)
+{
+    wallet -= amount;
+}
+
+void Client::addToWallet(double amount)
+{
+    wallet += amount;
+}
+
+void Client::deductPoints(unsigned points)
+{
+    points -= points;
+}
+
+void Client::addPoints(unsigned points)
+{
+    points += points;
+}
+
+void Client::executeCommand(Command* command)
+{
+    command->execute();
+}
