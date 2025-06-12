0eee29c27f6bcfdbf8c3b008afee550e8ba1e8ea
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-12 19:36:43 +0300

    Update BusinessProfile.h

--- ModaElectronicCommerceSystem/BusinessProfile.h
+++ ModaElectronicCommerceSystem/BusinessProfile.h
@@ -1,7 +1,40 @@
 #pragma once
+#include "User.h"
+#include "MyString.h"
+#include "MyVector.hpp"
+#include "Item.h"
+#include "Order.h"
+#include "Role.h"
+#include "Command.h"
 
-class BusinessProfile
+class BusinessProfile: public User
 {
+	MyVector<Item> items;
+	MyVector<Order> orders;
+	double revenue = 0.0;
+	Role role;
 
+public:
+	BusinessProfile(const MyString& username, const MyString& egn, const MyString& password, Role role = Role::Bussiness);
+
+	void addItem(const MyString& name, double price, unsigned quantity, const MyString& description);
+	void removeItem(const MyString& name);
+	void listItems() const;
+
+	void listPendingOrders() const;
+	void approveOrder(size_t index);
+	void rejectOrder(size_t index, const MyString& reason);
+	void listOrders() const;
+
+	void listBestSellers() const;
+	void readRevenue() const;
+
+	const MyString& getUsername() const;
+	double getRevenue() const;
+	Role getRole() const;
+
+	void addOrder(const Order& order);
+
+	virtual void executeCommand(Command* command);
 };
 
