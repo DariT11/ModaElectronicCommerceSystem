eb971d548cb1e1a9377bb77bcd9d14b2ace12fee
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-12 19:36:47 +0300

    Update BusinessProfile.cpp

--- ModaElectronicCommerceSystem/BusinessProfile.cpp
+++ ModaElectronicCommerceSystem/BusinessProfile.cpp
@@ -1,1 +1,167 @@
 #include "BusinessProfile.h"
+using namespace std;
+
+BusinessProfile::BusinessProfile(const MyString& username, const MyString& egn, const MyString& password, Role role)
+    :User(username,egn,password), role(role)
+{
+
+}
+
+void BusinessProfile::addItem(const MyString& name, double price, unsigned quantity, const MyString& description)
+{
+    items.push_back(Item(name, description, price, quantity));
+    cout << "Item: " << name << "is added! :)" << endl;
+}
+
+void BusinessProfile::removeItem(const MyString& name)
+{
+    for (size_t i = 0; i < items.getSize(); i++)
+    {
+        if (items.operator[](i).getName() == name)
+        {
+            items.erase(i);
+            cout << "Item: " << name << "was removed! :)" << endl;
+            return;
+        }
+    }
+    throw invalid_argument("Item with this name was not found! :( Please try again!");
+}
+
+void BusinessProfile::listItems() const
+{
+    if (items.empty())
+    {
+        cout << "There is no items yet! :(" << endl;
+        return;
+    }
+
+    for (size_t i = 0; i < items.getSize(); i++)
+    {
+        cout << "[" << i + 1 << "] Item:" << items.operator[](i).getName() << endl;
+        cout << "Price: " << items.operator[](i).getPrice() << endl;
+        cout << "Quantity: " << items.operator[](i).getQuantity() << endl;
+        cout << "Description: " << items.operator[](i).getDescription() << endl;
+    }
+}
+
+void BusinessProfile::listPendingOrders() const
+{
+    for (size_t i = 0; i < orders.getSize(); i++)
+    {
+        if (orders.operator[](i).getOrderStatus() == OrderStatus::Pending)
+        {
+            cout << i << ": ";
+            orders.operator[](i).print();
+        }
+    }
+}
+
+void BusinessProfile::approveOrder(size_t index)
+{
+    if (index >= orders.getSize())
+    {
+        throw out_of_range("Invalid order number! :( Please try again!");
+    }
+
+    Order& order = orders.operator[](index);
+    if (order.getOrderStatus() != OrderStatus::Pending)
+    {
+        cout << "The status of this order is not pending! :)" << endl;
+        return;
+    }
+
+    order.markAsShipped();
+    revenue += order.getPrice();
+
+    cout << "Order was approved and shipped! :)" << endl;
+}
+
+void BusinessProfile::rejectOrder(size_t index, const MyString& reason)
+{
+    if (index >= orders.getSize())
+    {
+        throw out_of_range("Invalid order number! :( Please try again!");
+    }
+
+    Order& order = orders.operator[](index);
+    if (order.getOrderStatus() != OrderStatus::Pending)
+    {
+        cout << "The status of this order is not pending! :)" << endl;
+        return;
+    }
+
+    // Тук трябва да се върнат пари и точки на клиента
+    // Това може да се реализира чрез връзка към Client (ако имаш такава)
+
+    orders.erase(index);
+    cout << "This order was cancelled and deleted, because of: " << reason << " :(" << endl;
+}
+
+void BusinessProfile::listOrders() const
+{
+    for (size_t i = 0; i < orders.getSize(); i++)
+    {
+        cout << "[" << i + 1<< "]";
+        orders.operator[](i).print();
+    }
+}
+
+void BusinessProfile::listBestSellers() const
+{
+    if (items.empty())
+    {
+        cout << "There are still no items in this business account! :(" << endl;
+        return;
+    }
+
+    MyVector<Item> sorted = items;
+    for (size_t i = 0; i < sorted.getSize(); i++)
+    {
+        for (size_t j = i + 1; j < sorted.getSize(); j++)
+        {
+            if (sorted.operator[](j).getSalesCount() > sorted.operator[](i).getSalesCount())
+            {
+                Item temp = sorted.operator[](i);
+                sorted.operator[](i) = sorted.operator[](j);
+                sorted.operator[](j) = temp;
+            }
+        }
+    }
+
+    cout << "Bestsellers: " << endl;
+    for (size_t i = 0; i < sorted.getSize(); i++)
+    {
+        cout << "Item: " << sorted.operator[](i).getName() << "is bought: " << sorted.operator[](i).getSalesCount()
+            << "times!";
+    }
+}
+
+void BusinessProfile::readRevenue() const
+{
+    cout << "Revenue: " << revenue << "BGN" << endl;
+}
+
+const MyString& BusinessProfile::getUsername() const
+{
+    return User::getUsername();
+}
+
+double BusinessProfile::getRevenue() const
+{
+    return revenue;
+}
+
+Role BusinessProfile::getRole() const
+{
+    return role;
+}
+
+void BusinessProfile::addOrder(const Order& order)
+{
+    orders.push_back(order);
+}
+
+void BusinessProfile::executeCommand(Command* command)
+{
+    command->execute();
+}
