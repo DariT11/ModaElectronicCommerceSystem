eaf3fc67577a34ff016eef13eb790f7e74408b82
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-12 14:18:08 +0300

    Update Order.cpp

--- ModaElectronicCommerceSystem/Order.cpp
+++ ModaElectronicCommerceSystem/Order.cpp
@@ -1,1 +1,84 @@
 #include "Order.h"
+using namespace std;
+
+unsigned Order::nextOrderId = 1;
+
+Order::Order(const Cart& cart)
+    :orderId(nextOrderId++), cart(cart), price(cart.calculateTotalWithDiscount()), status(OrderStatus::Pending), createdAt(time(nullptr))
+{
+
+}
+
+unsigned Order::getId() const
+{
+    return orderId;
+}
+
+double Order::getPrice() const
+{
+    return price;
+}
+
+unsigned Order::getRewardPoints() const
+{
+    return rewardPoints;
+}
+
+OrderStatus Order::getOrderStatus() const
+{
+    return status;
+}
+
+time_t Order::getCreationTime() const
+{
+    return createdAt;
+}
+
+void Order::markAsShipped()
+{
+    if (status == OrderStatus::Pending)
+    {
+        status = OrderStatus::Shipped;
+    }
+}
+
+void Order::markAsDelivered()
+{
+    if (status == OrderStatus::Shipped)
+    {
+        status = OrderStatus::Delivered;
+    }
+    //da izchislq tochkite
+}
+
+bool Order::isShipped() const
+{
+    return status == OrderStatus::Shipped;
+}
+
+bool Order::isDelivered() const
+{
+    return status == OrderStatus::Delivered;
+}
+
+void Order::print() const
+{
+    cout << "Îrder number: " << orderId << endl;
+    cout << "Îrder price: " << orderId << "BGN" << endl;
+    cout << "Reward points: " << rewardPoints << endl;
+    cout << "Order status: " << statusToString() << endl;
+    cout << "Order created at: " << ctime(&createdAt) << endl;
+    cout << "Order items: " << endl;
+    cart.view();
+}
+
+const char* Order::statusToString() const
+{
+    switch (status)
+    {
+    case OrderStatus::Pending: return "Pending"; break;
+    case OrderStatus::Shipped: return "Shipped"; break;
+    case OrderStatus::Delivered: return "Delivered"; break;
+    default: return "Status Unknown!"; break;
+    }
+}
