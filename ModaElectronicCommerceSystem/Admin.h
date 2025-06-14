4cb32e19ff822bc30b65b916a3438d4bebaa09ef
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-14 13:13:24 +0300

    Create Admin.h

---
+++ ModaElectronicCommerceSystem/Admin.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "User.h"
+#include "MyVector.hpp"
+#include "Transaction.h"
+#include "Check.h"
+#include "Client.h"
+
+class Admin : public User
+{
+	MyVector<Transaction> transactions;
+	MyVector<Check> issuedChecks;
+
+public:
+	Admin(const MyString& username, const MyString& egn, const MyString& password);
+	
+	void sendCheck(double amount, const MyString& code, Client& client);
+	void viewTransactions() const;
+	void clientInsights(const MyVector<Client>& clients) const;
+	void addTransaction(const Transaction& transaction);
+};
+
