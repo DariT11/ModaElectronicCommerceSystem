bac33b58256a79c51b3714146734d70808732715
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-14 16:04:54 +0300

    Update ModaElectronicCommerceSystem.cpp

--- ModaElectronicCommerceSystem/ModaElectronicCommerceSystem.cpp
+++ ModaElectronicCommerceSystem/ModaElectronicCommerceSystem.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "System.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Welcome to Darby-Moda online shop! :)" << endl;
+    cout << endl;
+
+    System system;
+    system.start();
 }
