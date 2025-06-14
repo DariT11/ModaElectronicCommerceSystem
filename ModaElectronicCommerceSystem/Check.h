b17f37ac50b955bd87fb5ead9ae4bf1b12a2297d
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-14 13:13:19 +0300

    Create Check.h

---
+++ ModaElectronicCommerceSystem/Check.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "MyString.h"
+
+class Check
+{
+	MyString code;
+	double amount;
+
+public:
+	Check() = default;
+	Check(const MyString& code, double amount);
+
+	const MyString& getCode() const;
+	double getAmount() const;
+
+	//setters
+};
+
