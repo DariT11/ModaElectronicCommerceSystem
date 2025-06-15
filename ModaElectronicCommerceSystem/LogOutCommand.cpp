af89f0124b1c6c5e68f2def1812ba35589276111
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 21:32:54 +0300

    Update LogOutCommand.cpp

--- ModaElectronicCommerceSystem/LogOutCommand.cpp
+++ ModaElectronicCommerceSystem/LogOutCommand.cpp
@@ -1,14 +1,21 @@
 #include "LogOutCommand.h"
+using namespace std;
 
-LogOutCommand::LogOutCommand(User* user)
+/*LogOutCommand::LogOutCommand(User* user)
 	:user(user)
 {
-}
+}*/
 
-void LogOutCommand::execute()
+void LogOutCommand::execute(System& system)
 {
+	User* user = system.getCurrentUser();
 	if (user)
 	{
 		user->logÎut();
+		system.setCurrentUser(nullptr);
+	}
+	else
+	{
+		cout << "No user is logged in! :(" << endl;
 	}
 }
