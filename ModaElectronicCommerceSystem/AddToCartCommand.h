8ebea87a67a8ed44911aadcaaae3b18901804e0f
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 21:34:46 +0300

    Update AddToCartCommand.h

--- ModaElectronicCommerceSystem/AddToCartCommand.h
+++ ModaElectronicCommerceSystem/AddToCartCommand.h
@@ -2,6 +2,7 @@
 #include "Command.h"
 #include "Client.h"
 #include "ItemsCatalog.h"
+class System;
 
 class AddToCartCommand: public Command
 {
@@ -12,6 +13,6 @@ class AddToCartCommand: public Command
 
 public:
 	AddToCartCommand(Client& client, ItemsCatalog& catalog, unsigned itemId, unsigned quantity);
-	void execute() override;
+	void execute(System& system) override;
 };
 
