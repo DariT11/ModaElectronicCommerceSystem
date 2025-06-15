7466028e619c266e8f51c60b5f1aa5962f2f6b6d
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 21:33:44 +0300

    Update CommandFactory.cpp

--- ModaElectronicCommerceSystem/CommandFactory.cpp
+++ ModaElectronicCommerceSystem/CommandFactory.cpp
@@ -1,1 +1,23 @@
 #include "CommandFactory.h"
+#include "LogOutCommand.h"
+#include "HelpCommand.h"
+
+CommandFactory& CommandFactory::getInstance()
+{
+    static CommandFactory commandFactory;
+    return commandFactory;
+}
+
+Command* CommandFactory::createCommand(const MyString& command)
+{
+    if (command == "logout")
+    {
+        return new LogOutCommand();
+    }
+    else if (command == "help") 
+    {
+        return new HelpCommand();
+    }
+
+    return nullptr;
+}
