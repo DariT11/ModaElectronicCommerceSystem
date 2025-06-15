54d2b0575d7c469193963cb0216c098d80e8cec9
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 21:32:22 +0300

    Update User.h

--- ModaElectronicCommerceSystem/User.h
+++ ModaElectronicCommerceSystem/User.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "Constants.h"
 #include "MyString.h"
+#include "Role.h"
+#include <fstream>
+
 class Command;
+class System;
 using namespace Constants;
 
 class User
@@ -10,15 +14,19 @@ protected:
 	MyString username;
 	MyString egn;
 	MyString password;
+	Role role;
 
 public:
-	User(const MyString& username, const MyString& egn, const MyString& password);
+	User(Role role);
+	User(const MyString& username, const MyString& egn, const MyString& password, Role role);
 
 	virtual ~User() = default;
 
 	virtual void logÎut(); 
 	virtual void help() const;
 	virtual void viewProfile() const;
+	virtual void serialize(std::ofstream& out) const;
+	virtual void deserialize(std::ifstream& in);
 
 	void setUsername(const MyString& username);
 	void setEgn(const MyString& egn);
@@ -27,6 +35,7 @@ public:
 	const MyString& getUsername() const;
 	const MyString& getEgn() const;
 	const MyString& getPassword() const;
+	Role getRole() const;
 
-	void executeCommand(Command* command);
+	void executeCommand(Command* command, System& system);
 };
