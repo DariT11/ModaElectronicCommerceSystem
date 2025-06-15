ad94c95accd041df7ab774b0ecfbf933df158e4f
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 13:00:07 +0300

    Update FilterByPriceCommand.cpp

--- ModaElectronicCommerceSystem/FilterByPriceCommand.cpp
+++ ModaElectronicCommerceSystem/FilterByPriceCommand.cpp
@@ -1,12 +1,11 @@
 #include "FilterByPriceCommand.h"
 
-FilterByPriceCommand::FilterByPriceCommand(ProductManager& pManager, Item** items, size_t itemsCount, bool ascending)
-	:pManager(pManager), items(items), itemsCount(itemsCount), ascending(ascending)
+FilterByPriceCommand::FilterByPriceCommand(ItemsCatalog& catalog)
+	:catalog(catalog)
 {
-
 }
 
 void FilterByPriceCommand::execute()
 {
-	pManager.filterByPrice(items, itemsCount, ascending);
+	catalog.filterByPrice();
 }
