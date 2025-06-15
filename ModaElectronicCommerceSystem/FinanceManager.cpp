a2e297b61ad855048ddbb358deb3e28d03e90e15
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 12:59:56 +0300

    Update FinanceManager.cpp

--- ModaElectronicCommerceSystem/FinanceManager.cpp
+++ ModaElectronicCommerceSystem/FinanceManager.cpp
@@ -1,9 +1,13 @@
 #include "FinanceManager.h"
+using namespace std;
 
 void FinanceManager::checkBalance(const Client& client) const
 {
+	cout << "Client balance: " << client.getWallet() << " BGN" << endl;
+	cout << "Client points: " << client.getPoints() << endl;
 }
 
-void FinanceManager::redeem(Client& client, const MyString& code) const
+void FinanceManager::redeem(Client& client, const MyString& code, Admin& admin) const
 {
+	client.redeemCheck(code, admin);
 }
