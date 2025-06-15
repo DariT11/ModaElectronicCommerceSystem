6b8ceca65c1a31daf273e9281578954588de6536
Darina <116870035+DariT11@users.noreply.github.com>
2025-06-15 21:32:51 +0300

    Update LogOutCommand.h

--- ModaElectronicCommerceSystem/LogOutCommand.h
+++ ModaElectronicCommerceSystem/LogOutCommand.h
@@ -1,13 +1,14 @@
 #pragma once
 #include "Command.h"
 #include "User.h"
+#include "System.h"
 
 class LogOutCommand: public Command
 {
-	User* user;
+	//User* user;
 
 public:
-	LogOutCommand(User* user);
-	void execute() override;
+	//LogOutCommand(User* user);
+	void execute(System& system) override;
 };
 
